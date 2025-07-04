ee9497bcc1d968638a03f04f1ce7e62d01572c30
J Ananya <ananya020406@gmail.com>
2025-07-04 13:42:55 +0530

    Initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="text-center p-8">
+      <h1 className="text-3xl font-bold">Welcome to Simulation Platform</h1>
+    </div>
+  );
+}
+
+export default App;
